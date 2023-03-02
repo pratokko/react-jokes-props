18a0dd87f9df08fb9254f33571dd157465044dcb
pratokko <pratokko3@gmail.com>
2023-03-02 22:32:40 +0300

    added a jokes data file as store

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Jokes from "./components/Jokes";
+import jokesData from "./components/JokesData";
 
 function App() {
-  return (
-    <div className="App">
-      <Jokes
-        setup="i got my daughter a fridge for her birthday."
-        punchline="I cant wait to see her face light up when she opens it"
-      />
-      <Jokes
-        setup="how did the hacker escape the police"
-        punchline=" he just runsomeware"
-      />
-      <Jokes
-        setup="why dont pirates travel on mountain roads"
-        punchline="Scurvy"
-      />
-      <Jokes
-        setup="why do bees stay in the hive in the winter"
-        punchline="Swarm"
-      />
+  const jokeElements = jokesData.map((joke) => (
+    <Jokes setup={joke.setup} punchline={joke.punchline} />
+  ));
 
-      <Jokes
-        setup="Whats the best thing about swizerland?"
-        punchline="i dont know but the flag is a big flag"
-      />
-    </div>
-  );
+  return <div className="App">{jokeElements}</div>;
 }
 
 export default App;
+
+{
+  /* <Jokes
+                                          setup="i got my daughter a fridge for her birthday."
+                                          punchline="I cant wait to see her face light up when she opens it"
+                                          />
+                                          <Jokes
+                                          setup="how did the hacker escape the police"
+                                          punchline=" he just runsomeware"
+                                          />
+                                          <Jokes
+                                          setup="why dont pirates travel on mountain roads"
+                                          punchline="Scurvy"
+                                          />
+                                          <Jokes
+                                          setup="why do bees stay in the hive in the winter"
+                                          punchline="Swarm"
+                                          />
+
+                                          <Jokes
+                                          setup="Whats the best thing about swizerland?"
+                                          punchline="i dont know but the flag is a big flag"
+                                          /> */
+}
---
+++ src/components/JokesData.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    setup: "i got my daughter a fridge for her birthday.",
+    punchline: "I cant wait to see her face light up when she opens it",
+  },
+  {
+    setup: "how did the hacker escape the police",
+    punchline: " he just runsomeware",
+  },
+  {
+    setup: "why dont pirates travel on mountain roads",
+    punchline: "Scurvy",
+  },
+  {
+    setup: "why do bees stay in the hive in the winter",
+    punchline: "Swarm",
+  },
+  {
+    setup: "Whats the best thing about swizerland?",
+    punchline: "i dont know but the flag is a big flag",
+  },
+];
