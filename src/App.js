041631749b6511457d960d5de609d7f319e3e332
pratokko <pratokko3@gmail.com>
2023-03-02 20:08:14 +0300

    react- jokes using props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Jokes from "./components/Jokes";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Jokes
+        setup="i got my daughter a fridge for her birthday."
+        punchline="I cant wait to see her face light up when she opens it"
+      />
+      <Jokes
+        setup="how did the hacker escape the police"
+        punchline=" he just runsomeware"
+      />
+      <Jokes
+        setup="why dont pirates travel on mountain roads"
+        punchline="Scurvy"
+      />
+      <Jokes
+        setup="why do bees stay in the hive in the winter"
+        punchline="Swarm"
+      />
+
+      <Jokes
+        setup="Whats the best thing about swizerland?"
+        punchline="i dont know but the flag is a big flag"
+      />
     </div>
   );
 }
---
+++ src/components/Jokes.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Jokes(props) {
+  return (
+    <div>
+        <h2> {props.setup}</h2>
+        <p>   {props.punchline} </p>
+        <hr />
+    </div>
+  )
+}
+
+export default Jokes
